ec47b3693dc7ae754d80e534a71da007fa83eddf
optimistiks <mail@potapovmax.com>
2015-07-25 12:12:30 +0300

    add translation_history to /translation response, closes #2

--- ezdict/translation_history/serializers.py
+++ ezdict/translation_history/serializers.py
@@ -8,7 +8,6 @@ class TranslationHistorySerializer(serializers.ModelSerializer):
         model = TranslationHistory
 
     user = serializers.PrimaryKeyRelatedField(read_only=True, default=None)
-    count = serializers.IntegerField(read_only=True)
 
     def validate_user(self, value):
         return self.context['request'].user
