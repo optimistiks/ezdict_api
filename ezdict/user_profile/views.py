859ceb4b42d7883e3fef7bd3d5dc0213cac94cd9
optimistiks <mail@potapovmax.com>
2015-08-15 11:24:49 +0300

    user profile editing (optimistiks/ezdict_chrome_ext#22)

--- ezdict/user_profile/views.py
+++ ezdict/user_profile/views.py
@@ -1,16 +1,26 @@
-from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 from models import UserProfile
 from serializers import UserProfileSerializer
+from rest_framework import generics
 
 
-class UserProfileView(APIView):
+class UserProfileView(generics.GenericAPIView):
     """
     View to control user profile
     """
 
-    def get(self, request):
+    serializer_class = UserProfileSerializer
+
+    def get(self, request, *args, **kwargs):
         profile = UserProfile.findByUser(request.user)
         serializer = UserProfileSerializer(profile)
         return Response(serializer.data, status=status.HTTP_200_OK)
+
+    def post(self, request, *args, **kwargs):
+        profile = UserProfile.findByUser(request.user)
+        serializer = UserProfileSerializer(profile, data=request.data, context={'request': request})
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
