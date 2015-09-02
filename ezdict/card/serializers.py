53a4c771c09925ac88acabac35d4206779e3947e
optimistiks <mail@potapovmax.com>
2015-09-02 08:53:15 +0300

    add serializer & update messages (#13)

--- ezdict/card/serializers.py
+++ ezdict/card/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from models import Card
+from models import Card, CardMeaning
 from rest_framework.validators import UniqueTogetherValidator
 from django.utils.translation import ugettext as _
 
@@ -12,6 +12,23 @@ class CardSerializer(serializers.ModelSerializer):
                 queryset=Card.objects.all(),
                 fields=('user', 'text'),
                 message=_('Card for this text already exists.')
+            )
+        ]
+
+    user = serializers.PrimaryKeyRelatedField(read_only=True, default=None)
+
+    def validate_user(self, value):
+        return self.context['request'].user
+
+
+class CardMeaningSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = CardMeaning
+        validators = [
+            UniqueTogetherValidator(
+                queryset=CardMeaning.objects.all(),
+                fields=('card', 'text'),
+                message=_('This meaning already exists.')
             )
         ]
 
