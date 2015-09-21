a66f45f8c8c6ddedc5c434af4ef66d6644adb0a0
Potapov Max <mpmsp@yandex-team.ru>
2015-09-21 18:53:36 +0300

    create quizzes with quizCards (tests are failing) (#15)

--- ezdict/quiz/tests.py
+++ ezdict/quiz/tests.py
@@ -3,6 +3,7 @@ from rest_framework import status
 from rest_framework.test import APITestCase
 from django.contrib.auth import get_user_model
 from ezdict.quiz.models import Quiz
+from ezdict.card.models import Card, CardToStudy
 
 
 class QuizTests(APITestCase):
@@ -11,8 +12,26 @@ class QuizTests(APITestCase):
             username='test', email='test@test.com', password='test')
         self.client.force_authenticate(user=self.user)
 
-    def testQuizIsCreated(self):
+    def testQuizIsNotCreatedIfThereAreNoCardsAndErrorIsThrown(self):
         url = reverse('quiz-list')
 
         response = self.client.post(url)
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+
+    def testQuizIsCreatedWithSetOfQuizCards(self):
+        url = reverse('quiz-list')
+
+        card = Card()
+        card.user = self.user
+        card.text = 'hello'
+        card.save()
+
+        toStudy = CardToStudy()
+        toStudy.card = card
+        toStudy.user = self.user
+        toStudy.save()
+
+        response = self.client.post(url)
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertIsNotNone(response.data.quiz_cards)
+
--- ezdict/quiz/views.py
+++ ezdict/quiz/views.py
@@ -1,16 +1,21 @@
-from models import Quiz
+from models import Quiz, QuizCard
+from ezdict.card.models import Card
 from serializers import QuizSerializer
 from rest_framework.viewsets import ModelViewSet
 from rest_framework_bulk import (
     BulkModelViewSet,
 )
-from rest_framework import mixins
+from rest_framework import mixins, serializers
 from rest_framework.viewsets import GenericViewSet
+from django.utils.translation import ugettext as _
+from django.db import transaction
+from django.db.models import Q
 
 
 class QuizViewSet(mixins.CreateModelMixin,
                   mixins.RetrieveModelMixin,
                   mixins.ListModelMixin,
+                  mixins.DestroyModelMixin,
                   GenericViewSet):
     """
     A ViewSet for working with quizzes.
@@ -18,3 +23,22 @@ class QuizViewSet(mixins.CreateModelMixin,
     queryset = Quiz.objects.all()
     serializer_class = QuizSerializer
     ordering = ('-created',)
+
+    @transaction.atomic
+    def perform_create(self, serializer):
+        cards = Card.objects.filter(
+            Q(quiz_cards__isnull=True) | Q(quiz_cards__quiz__completed__isnull=False),
+            to_study__isnull=False,
+            user_id__exact=self.request.user.id
+        )
+        if cards:
+            serializer.save()
+            for card in cards:
+                quizCard = QuizCard()
+                quizCard.user = self.request.user
+                quizCard.card = card
+                quizCard.quiz = serializer.instance
+                quizCard.save()
+        else:
+            raise serializers.ValidationError(_('There are no valid cards for quiz.'))
+
